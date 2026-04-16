5ebfbca38805699dac39282903a34b9f618ab6b0
Perla Couoh <perla.couoh_pm25247@cecyte.edu.mx>
2026-04-16 11:24:20 -0600

    Estructura inicial del proyecto

---
+++ main.py
@@ -0,0 +1,8 @@
+def main():
+    print("--- Analizador de datos v1,0 ---")
+    datos = [10,20,30,40, 50,60] #datos de prueba
+    print("Datos a procesar: {datos}")
+
+    #Aqui se llamaran las funciones de los integrantes del equipo
+if __name__ =="__main__":
+    main()
